a907582f329caa3d4b29632795569da1d9edf2d9
Darvand <david.montoya@rootstrap.com>
2022-07-19 11:34:44 -0500

    Add Vite boilerplate. Add tailwindcss. Add initial colors

---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{ts,tsx}"],
+  theme: {
+    colors: {
+      "primary-dark": "#292D39",
+      "secondary-dark": "#222733",
+      "third-dark": "#212A39",
+      "light-gray": "#B0B3BD",
+      gray: "#54586C",
+      "dark-gray": "#696C76",
+      white: "#ffffff",
+      blue: "#1D5EFF",
+      "light-blue": "#21366A",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
